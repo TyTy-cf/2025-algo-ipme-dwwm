2ee5092eeb76765681cbc1ce82ff0bbdde6e5c00
Joulos-dev <jules.fournol@hotmail.com>
2025-05-20 13:40:23 +0200

    deuxième tentative

--- exo_algo_js.md
+++ exo_algo_js.md
@@ -1,4 +1,3 @@
-
 ## Exercices
 
 Faites une fonction par exercices...
@@ -9,10 +8,11 @@ Faites une fonction par exercices...
 
 Il demande à l'utilisateur de donner une heure et des minutes (séparemment)
 
-Si l'utilisateur saisie : 
-- Heure : 11
-- Minutes : 13
-L'algo doit écrire : "Dans 1 minute il sera 11h14"
+Si l'utilisateur saisie :
+
+-   Heure : 11
+-   Minutes : 13
+    L'algo doit écrire : "Dans 1 minute il sera 11h14"
 
 (Bonus : pour ceux qui trouve comment afficher réellement qu'il est 11h14, 1 min après !)
 
@@ -20,8 +20,7 @@ L'algo doit écrire : "Dans 1 minute il sera 11h14"
 
 La fonction ici, prendra en paramètre le nombre de copie
 Elle doit calculer le prix de ces copies :
-- S'il y a moins ou 10 copies, alors le prix unitaire est de 0.10€
-- S'il y a moins ou 50 copies, alors le prix unitaire est de 0.09€
-- S'il y a plus ou 100 copies, alors le prix unitaire est de 0.08€
 
-
+-   S'il y a moins ou 10 copies, alors le prix unitaire est de 0.10€
+-   S'il y a moins ou 50 copies, alors le prix unitaire est de 0.09€
+-   S'il y a plus ou 100 copies, alors le prix unitaire est de 0.08€
--- julesExo.js
+++ julesExo.js
@@ -1,28 +1,44 @@
 //1//
 
-let heure = prompt("Quel heure est-il");
-let minutes = prompt("Combien de minutes ?");
-heure = parseInt(heure);
-minutes = parseInt(minutes);
-minutes = minutes + 1;
+function predictFuture() {
+    let heure = prompt("Quel heure est-il");
+    let minutes = prompt("Combien de minutes ?");
+    heure = parseInt(heure);
+    minutes = parseInt(minutes);
 
-console.log("Dans 1 minutes il sera " + heure + "h" + minutes);
+    minutes = minutes + 1;
+    if (minutes === 60) {
+        minutes = 0;
+        heure++;
+    }
+    if (heure === 24) {
+        heure = 0;
+    }
+
+    console.log("Dans 1 minutes il sera " + heure + "h" + minutes);
+}
+
+predictFuture();
 
 //2//
 
-let nbCopie = prompt("Indiquez le nombre de copie ?");
-nbCopie = parseInt(nbCopie);
-let prixUnitaire = 1;
+function calculByPrice() {
+    let nbCopie = prompt("Indiquez le nombre de copie ?");
+    nbCopie = parseInt(nbCopie);
+    let prixUnitaire = 1;
 
-if (nbCopie <= 10) {
-    prixUnitaire = 0.1;
-    console.log("Le prix unitaire des copies est de 0.10€ ");
-} else if (nbCopie <= 100) {
-    prixUnitaire = 0.08;
-    console.log("Le prix unitaire des copies est de 0.08€ ");
-} else {
-    prixUnitaire = 0.09;
-    console.log("Le prix unitaire des copies est de 0.09€ ");
+    if (nbCopie <= 10) {
+        prixUnitaire = 0.1;
+        console.log("Le prix unitaire des copies est de 0.10€ ");
+    } else if (nbCopie <= 100) {
+        prixUnitaire = 0.08;
+        console.log("Le prix unitaire des copies est de 0.08€ ");
+    } else {
+        prixUnitaire = 0.09;
+        console.log("Le prix unitaire des copies est de 0.09€ ");
+    }
+    let prixTotal = prixUnitaire * nbCopie;
+    console.log("Le prix total des copies est de " + prixTotal + "€");
 }
-let prixTotal = prixUnitaire * nbCopie;
-console.log("Le prix total des copies est de " + prixTotal + "€");
+
+calculByPrice();
