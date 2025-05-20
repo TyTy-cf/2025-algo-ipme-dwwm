41ed72df9f219f26462e16b3333b81042c81ef14
Joulos-dev <jules.fournol@hotmail.com>
2025-05-20 13:24:14 +0200

    exo algo 1 et 2

--- julesExo.js
+++ julesExo.js
@@ -2,6 +2,8 @@
 
 let heure = prompt("Quel heure est-il");
 let minutes = prompt("Combien de minutes ?");
+heure = parseInt(heure);
+minutes = parseInt(minutes);
 minutes = minutes + 1;
 
 console.log("Dans 1 minutes il sera " + heure + "h" + minutes);
@@ -9,11 +11,18 @@ console.log("Dans 1 minutes il sera " + heure + "h" + minutes);
 //2//
 
 let nbCopie = prompt("Indiquez le nombre de copie ?");
+nbCopie = parseInt(nbCopie);
+let prixUnitaire = 1;
 
 if (nbCopie <= 10) {
+    prixUnitaire = 0.1;
     console.log("Le prix unitaire des copies est de 0.10€ ");
 } else if (nbCopie <= 100) {
+    prixUnitaire = 0.08;
     console.log("Le prix unitaire des copies est de 0.08€ ");
 } else {
+    prixUnitaire = 0.09;
     console.log("Le prix unitaire des copies est de 0.09€ ");
 }
+let prixTotal = prixUnitaire * nbCopie;
+console.log("Le prix total des copies est de " + prixTotal + "€");
